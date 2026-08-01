146b9f20764865a06aa7c84da421cc1e94906361
richdevzz <richardbrown1315@gmail.com>
2026-08-01 00:05:15 -0500

    Proyecto Iniciado

---
+++ memory-space/src/js/messages.js
@@ -0,0 +1,8 @@
+export const messages = [
+    "Eres mi lugar favorito ❤️",
+    "Gracias por existir.",
+    "Te volvería a elegir mil veces.",
+    "Contigo todo tiene sentido.",
+    "Siempre tú.",
+    "Te amo muchísimo."
+];
